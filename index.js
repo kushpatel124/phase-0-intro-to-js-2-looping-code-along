8ee99cc7eb1ae7ebc35b96f1dbe31623ba58cebe
Kush <kushpatel124@gmail.com>
2022-12-10 16:16:00 -0500

    commit tree f1d2c1f3386fc428209aae03db03881495393d78

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+  const messages = []
+  for (let i = 0; i < arr.length; i++) {
+
+    const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+    messages.push(greetingMessage)
+  }
+
+  return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Ali"], "surprise");
+
+
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  }
+}
